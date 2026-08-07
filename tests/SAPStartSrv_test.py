b83385b8024c1a974f2209b865ab1e158c9ead4a
Fabian Herschel <fabian.herschel@suse.com>
2026-08-07 11:44:13 +0200

    test/SAPStartSrv_test.py - try to fix test error for probe in method monitor() part5 (test only2)

--- tests/SAPStartSrv_test.py
+++ tests/SAPStartSrv_test.py
@@ -862,7 +862,7 @@ class TestSAPStartSrv(unittest.TestCase):
         ocf_returncode = self._agent.monitor()
         assert ocf_returncode == 0
 
-        if not mock_is_probe:
+        if mock_is_probe:
             self._agent._inititialize.assert_called_once_with()
             assert self._agent._get_status.call_count == 0
 
